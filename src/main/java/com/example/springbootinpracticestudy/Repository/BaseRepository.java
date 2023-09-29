c551a2bb808450e0d661c4f694102959086f6f38
Jaehoon Park <wido1593@gmail.com>
2023-09-29 23:47:02 +0900

    학습기록

--- src/main/java/com/example/springbootinpracticestudy/Repository/BaseRepository.java
+++ src/main/java/com/example/springbootinpracticestudy/Repository/BaseRepository.java
@@ -3,9 +3,9 @@ package com.example.springbootinpracticestudy.Repository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
-@NoRepositoryBean
+@NoRepositoryBean // 프록시 객체가 생성되지 않게
 public interface BaseRepository<T, ID>  extends Repository<T, ID> {
-
+    // 사용할 메서드만 표시하여 노출할 기능을 제한.
     <S extends T> S save(S entity);
 
     Iterable<T> findAll();
--- src/test/java/com/example/springbootinpracticestudy/SpringBootInPracticeStudyApplicationTest.java
+++ src/test/java/com/example/springbootinpracticestudy/SpringBootInPracticeStudyApplicationTest.java
@@ -1,25 +1,43 @@
 package com.example.springbootinpracticestudy;
 
 import com.example.springbootinpracticestudy.Model.Course;
+import com.example.springbootinpracticestudy.Repository.CourseRepository;
 import com.example.springbootinpracticestudy.Repository.CustomizedCourseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 class SpringBootInPracticeStudyApplicationTest {
 
     @Autowired
-    private CustomizedCourseRepository customizedCourseRepository;
+    private CourseRepository courseRepository;
 
     @Test
-    public void givenCreateCourseWhenFindAllCoursesThenExpectOneCourse() {
-        Course course = new Course("Rapid Spring Boot Application Development", "Spring", 4, "'Spring Boot gives all the power of the Spring Framework without all of the complexities");
-        customizedCourseRepository.save(course);
-        assertThat(Arrays.asList(customizedCourseRepository.findAll()).size()).isEqualTo(1);
+    public void givenCreateCourseWhenLoadTheCourseThenExpectSameCourse() {
+        courseRepository.saveAll(getCourseList());
+        assertThat(courseRepository.findAllByCategory("Spring")).hasSize(3);
+        assertThat(courseRepository.existsByName("JavaScript for All")).isTrue();
+        assertThat(courseRepository.existsByName("Mastering JavaScript")).isFalse();
+        assertThat(courseRepository.countByCategory("Python")).isEqualTo(2);
+        assertThat(courseRepository.findByNameStartsWith("Getting Started")).hasSize(3);
+    }
+
+    private List<Course> getCourseList() {
+        Course rapidSpringBootCourse = new Course("Rapid Spring Boot Application Development", "Spring", 4,"Spring Boot gives all the power of the Spring Framework without all of the complexity");
+        Course springSecurityDslCourse = new Course("Getting Started with Spring Security DSL", "Spring", 5, "Learn Spring Security DSL in easy steps");
+        Course springCloudKubernetesCourse = new Course("Getting Started with Spring Cloud Kubernetes", "Spring", 3, "Master Spring Boot application deployment with Kubernetes");
+        Course rapidPythonCourse = new Course("Getting Started with Python", "Python", 5, "Learn Python concepts in easy steps");
+        Course gameDevelopmentWithPython = new Course("Game Development with Python", "Python", 3, "Learn Python by developing 10 wonderful games");
+        Course javaScriptForAll = new Course("JavaScript for All", "JavaScript", 4, "Learn basic JavaScript syntax that can apply to anywhere");
+        Course javaScriptCompleteGuide = new Course("JavaScript Complete Guide", "JavaScript", 5, "Master JavaScript with Core Concepts and Web Development");
+
+        return Arrays.asList(rapidSpringBootCourse, springSecurityDslCourse, springCloudKubernetesCourse, rapidPythonCourse, gameDevelopmentWithPython, javaScriptForAll, javaScriptCompleteGuide);
     }
 }
