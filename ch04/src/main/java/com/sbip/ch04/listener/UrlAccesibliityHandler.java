433b1695790dd2ff62fc73d960510160f5a6223e
Jaehoon Park <wido1593@gmail.com>
2023-10-05 17:56:59 +0900

    학습기록 - 스프링 커스텀 액추에이터

--- ch04/src/main/java/com/sbip/ch04/listener/UrlAccesibliityHandler.java
+++ ch04/src/main/java/com/sbip/ch04/listener/UrlAccesibliityHandler.java
@@ -12,7 +12,7 @@ public class UrlAccesibliityHandler {
     @Value("${api.url:https://dog.ceo/}")
     private String url;
 
-    @EventListener(classes = ContextRefreshedEvent.class) // 초기화 단계에서 컨텍스트의 새로고침이 일어날 때.
+    //@EventListener(classes = ContextRefreshedEvent.class) // 초기화 단계에서 컨텍스트의 새로고침이 일어날 때.
     public void listen() {
         //항상 예외를 던지도록 작성.
         throw new UrlNotAccessibleException(url);
