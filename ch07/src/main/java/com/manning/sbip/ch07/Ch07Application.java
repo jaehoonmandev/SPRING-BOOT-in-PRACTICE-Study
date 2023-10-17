3e4d33dfe4d74d8d0f324c8cd15dd7948d30fd35
Jaehoon Park <wido1593@gmail.com>
2023-10-17 13:35:44 +0900

    ch07 학습기록 - API 문서화 > Open API 문서화 기능 이용

--- ch07/src/main/java/com/manning/sbip/ch07/Ch07Application.java
+++ ch07/src/main/java/com/manning/sbip/ch07/Ch07Application.java
@@ -1,7 +1,12 @@
 package com.manning.sbip.ch07;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Ch07Application {
@@ -10,4 +15,14 @@ public class Ch07Application {
         SpringApplication.run(Ch07Application.class, args);
     }
 
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+                                 @Value("${app.version}") String appVersion) {
+
+        return new OpenAPI().info(new Info().title("Course Tracker API").version(appVersion)
+                .description(appDescription).termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
 }
--- ch07/src/main/java/com/manning/sbip/ch07/controller/CourseController.java
+++ ch07/src/main/java/com/manning/sbip/ch07/controller/CourseController.java
@@ -2,17 +2,13 @@ package com.manning.sbip.ch07.controller;
 
 import java.util.Optional;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.manning.sbip.ch07.model.Course;
 import com.manning.sbip.ch07.service.CourseService;
@@ -24,6 +20,8 @@ import com.manning.sbip.ch07.service.CourseService;
 // @ResponseBody 애너테이션을 붙이면 반환값이 HTTP 응답 본문에 바인딩된다.
 @RestController
 @RequestMapping("/courses/") //API 엔드포인트 지정.
+//API를 문서화 하기 위한 애너테이션으로 컨트롤러에 대한 정보를 나타낸다.
+@Tag(name = "Course Controller", description = "This REST Controller provide services to manage courses in the Course Tracker application")
 public class CourseController {
 	
 	private CourseService courseService;
@@ -34,18 +32,25 @@ public class CourseController {
 	}
 
 	//'/courses/' 의 엔드포인트로 GET만 요청했을 시에는 모든 코스 정보를 제공한다.
+
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)// API가 반환하는 상태 코드. 이를통해 API 사용자가 로직을 구성하므로 신중하게 상태 코드를 결정하자.
+	@Operation(summary = "Provides all courses available in the Course Tracker application") // 엔드포인트의 목적
 	public Iterable<Course> getAllCourses() {
 		return courseService.getCourses();
 	}
 
 	//'/courses/{id}' 경로 변수를 지정.
 	@GetMapping("{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	@Operation(summary = "Provides course details for the supplied course id from the Course Tracker application")
 	public Optional<Course> getCourseById(@PathVariable("id") long courseId) {
 		return courseService.getCourseById(courseId);
 	}
 	
 	@GetMapping("category/{name}")
+	@ResponseStatus(code = HttpStatus.OK)
+	@Operation(summary = "Provides course details for the supplied course category from the Course Tracker application")
 	public Iterable<Course> getCourseByCategory(@PathVariable("name") String category) {
 		return courseService.getCoursesByCategory(category);
 	}
@@ -53,21 +58,29 @@ public class CourseController {
 	//HTTP POST 요청은 요청 본문(payload)가 있어야한다.
 	//이를 위해서 	@RequestBody로 요청 본문을 바인딩한다.
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@Operation(summary = "Creates a new course in the Course Tracker application")
 	public Course createCourse(@Valid @RequestBody Course course) {
 		return courseService.createCourse(course);
 	}
 	
 	@PutMapping("{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	@Operation(summary = "Updates the course details in the Course Tracker application for the supplied course id")
 	public void updateCourse(@PathVariable("id") long courseId, @Valid @RequestBody Course course) {
 		courseService.updateCourse(courseId, course);
 	}
 	
 	@DeleteMapping("{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	@Operation(summary = "Deletes the course details for the supplied course id from the Course Tracker application")
 	void deleteCourseById(@PathVariable("id") long courseId) {
 		courseService.deleteCourseById(courseId);
 	}
 	
 	@DeleteMapping
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	@Operation(summary = "Deletes all courses from the Course Tracker application")
 	void deleteCourses() {
 		courseService.deleteCourses();
 	}
